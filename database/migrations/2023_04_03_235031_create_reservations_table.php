9962b2bd9652a5468597a4d97660d607ad427f0a
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2023-04-04 11:10:43 +0000

    create database, Models and Controllers

---
+++ database/migrations/2023_04_03_235031_create_reservations_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('car_id');
+            $table->unsignedBigInteger('user_id');
+            $table->date('date_out');
+            $table->date('date_back');
+            $table->boolean('operation_confirmed')->default(false);
+            $table->timestamps();
+
+            $table->foreign('car_id')
+            ->references('id')
+            ->on('cars')
+            ->onDelete('cascade');
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
